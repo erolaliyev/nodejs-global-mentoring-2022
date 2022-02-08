12f913806606d14c359c9ecf70a99b6d7d01cd87
Erol Aliyev <erol_aliyev@epam.com>
2022-02-08 13:52:04 +0100

    Modify to import only needed methods

--- module-2/task3-2.js
+++ module-2/task3-2.js
@@ -1,12 +1,12 @@
-import fs from 'fs';
+import { createReadStream, createWriteStream } from 'fs';
 import csvtojson from 'csvtojson';
 
-const readStream = fs.createReadStream('./csv/nodejs-hw1-ex1.csv');
-const writeStream = fs.createWriteStream('./module-2/demo3-2.txt');
+const readStream = createReadStream('./csv/nodejs-hw1-ex1.csv');
+const writeStream = createWriteStream('./module-2/demo3-2.txt');
 
 readStream
-  .pipe(csvtojson())
-  .pipe(writeStream)
-  .on('error', (error) => {
-    console.log(error);
-  });
+	.pipe(csvtojson())
+	.pipe(writeStream)
+	.on('error', (error) => {
+		console.log(error);
+	});
