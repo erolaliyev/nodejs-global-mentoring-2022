5f73c67d5b6cd00f29d3741d3aad0dab7a6ab91c
Erol Aliyev <erol_aliyev@epam.com>
2022-02-23 19:57:44 +0100

    Fix eslint

--- module-3/home-task-2.js
+++ module-3/home-task-2.js
@@ -5,119 +5,121 @@ import Joi from 'joi';
 const app = express();
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const users = [];
 
 function validateUser(user) {
-  const schema = Joi.object({
-    login: Joi.string().required(),
-    password: Joi.string()
-      .pattern(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/)
-      .required(),
-    age: Joi.number().min(4).max(130).prefs({ convert: false }).required(),
-  });
+	const schema = Joi.object({
+		login: Joi.string().required(),
+		password: Joi.string()
+			.pattern(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/)
+			.required(),
+		age: Joi.number().min(4).max(130).prefs({ convert: false }).required(),
+	});
 
-  return schema.validate(user);
+	return schema.validate(user);
 }
 
 function getAutoSuggestUsers(loginSubstring, limit) {
-  return users
-    .filter((user) => user.login.includes(loginSubstring))
-    .sort((user1, user2) => (user1.login > user2.login ? 1 : -1))
-    .slice(0, limit);
+	return users
+		.filter((user) => user.login.includes(loginSubstring))
+		.sort((user1, user2) => (user1.login > user2.login ? 1 : -1))
+		.slice(0, limit);
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 app.get('/users', (req, res) => {
-  try {
-    return res.status(200).send(users);
-  } catch (error) {
-    return res.status(400).send({ error: error.toString() });
-  }
+	try {
+		return res.status(200).send(users);
+	} catch (error) {
+		return res.status(400).send({ error: error.toString() });
+	}
 });
 
 app.get('/users/suggested-users', (req, res) => {
-  try {
-    return res
-      .status(200)
-      .send(getAutoSuggestUsers(req.query.loginSubstring, req.query.limit));
-  } catch (error) {
-    return res.status(400).send({ error: error.toString() });
-  }
+	try {
+		return res
+			.status(200)
+			.send(getAutoSuggestUsers(req.query.loginSubstring, req.query.limit));
+	} catch (error) {
+		return res.status(400).send({ error: error.toString() });
+	}
 });
 
 app.get('/users/:id', (req, res) => {
-  try {
-    const foundUser = users.find((user) => user.id === req.params.id);
-    if (!foundUser) {
-      return res.status(404).send('The user with the given ID was not found!');
-    }
-    return res.status(200).send(foundUser);
-  } catch (error) {
-    return res.status(400).send({ error: error.toString() });
-  }
+	try {
+		const foundUser = users.find((user) => user.id === req.params.id);
+		if (!foundUser) {
+			return res.status(404).send('The user with the given ID was not found!');
+		}
+		return res.status(200).send(foundUser);
+	} catch (error) {
+		return res.status(400).send({ error: error.toString() });
+	}
 });
 
 app.post('/users', (req, res) => {
-  try {
-    const { error } = validateUser(req.body);
-    if (error) {
-      return res.status(400).send(error.details[0].message);
-    }
-    const user = {
-      id: uuidv4(),
-      login: req.body.login,
-      password: req.body.password,
-      age: req.body.age,
-      isDeleted: false,
-    };
-    users.push(user);
-    return res.status(200).send(user);
-  } catch (error) {
-    return res.status(400).send({ error: error.toString() });
-  }
+	try {
+		const { error } = validateUser(req.body);
+		if (error) {
+			return res.status(400).send(error.details[0].message);
+		}
+		const user = {
+			id: uuidv4(),
+			login: req.body.login,
+			password: req.body.password,
+			age: req.body.age,
+			isDeleted: false,
+		};
+		users.push(user);
+		return res.status(200).send(user);
+	} catch (error) {
+		return res.status(400).send({ error: error.toString() });
+	}
 });
 
 app.put('/users/:id', (req, res) => {
-  try {
-    const user = users.find((user) => user.id === req.params.id);
-    if (!user) {
-      return res.status(404).send('The user with given ID was not found!');
-    }
+	try {
+		const user = users.find((currentUser) => currentUser.id === req.params.id);
+		if (!user) {
+			return res.status(404).send('The user with given ID was not found!');
+		}
 
-    const { error } = validateUser(req.body);
-    if (error) {
-      return res.status(400).send(error.details[0].message);
-    }
+		const { error } = validateUser(req.body);
+		if (error) {
+			return res.status(400).send(error.details[0].message);
+		}
 
-    user.login = req.body.login;
-    user.password = req.body.password;
-    user.age = req.body.age;
+		user.login = req.body.login;
+		user.password = req.body.password;
+		user.age = req.body.age;
 
-    return res.status(200).send(user);
-  } catch (error) {
-    return res.status(400).send({ error: error.toString() });
-  }
+		return res.status(200).send(user);
+	} catch (error) {
+		return res.status(400).send({ error: error.toString() });
+	}
 });
 
 app.delete('/users/delete/:login', (req, res) => {
-  try {
-    const user = users.find((user) => user.login === req.params.login);
-    if (!user) {
-      return res.status(404).send('The user with given login was not found!');
-    }
+	try {
+		const user = users.find(
+			(currentUser) => currentUser.login === req.params.login
+		);
+		if (!user) {
+			return res.status(404).send('The user with given login was not found!');
+		}
 
-    if (user.isDeleted) {
-      return res
-        .status(400)
-        .send('The user with given login is already deleted!');
-    }
+		if (user.isDeleted) {
+			return res
+				.status(400)
+				.send('The user with given login is already deleted!');
+		}
 
-    user.isDeleted = true;
-    return res.status(200).send(user);
-  } catch (error) {
-    return res.status(400).send({ error: error.toString() });
-  }
+		user.isDeleted = true;
+		return res.status(200).send(user);
+	} catch (error) {
+		return res.status(400).send({ error: error.toString() });
+	}
 });
